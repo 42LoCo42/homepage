34d6f4eb28bf0c1f101624558893c513a86f520b
Leon Schumacher <leonsch@protonmail.com>
2023-03-02 09:15:00 +0100

    Add Element

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 GOARCH=arm64 CGO_ENABLED=0 go build .
-rsync -azvP --delete \
-	static \
-	stuff \
+rsync -azvLP --delete \
+	element.config.json \
 	index.html \
 	matrix.html \
-	tree.html \
 	new-site \
+	static \
+	stuff \
+	tree.html \
 	bunny:homepage/
+
+ssh bunny "cp homepage/element.config.json /var/www/html/element/config.json"
