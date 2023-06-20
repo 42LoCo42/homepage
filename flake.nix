e26062bc2d5145aa0f0e18b04112e55e010b1495
Leon Schumacher <leonsch@protonmail.com>
2023-06-20 23:32:02 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "devshell for my homepage";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+          ];
+        };
+      });
+}
